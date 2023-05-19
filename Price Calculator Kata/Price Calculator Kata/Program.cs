a7a2ef731370763cac8cf8e786750ba8c0c93b2a
Dima <dimaabujaber422000@gmail.com>
2023-05-19 17:23:03 +0300

    Report commit

--- Price Calculator Kata/Price Calculator Kata/Program.cs
+++ Price Calculator Kata/Price Calculator Kata/Program.cs
@@ -5,9 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Product Book = new Product("The Little Prince", 12345, 20.25);
-
-        Product BookWithTaxAndDiscount = Calculate.AddTaxWithDiscount(Book, 20, 15);
         Console.WriteLine("Sample product: Book with name = “The Little Prince”, UPC=12345, price=$20.25.");
-        Console.WriteLine($"Tax amount = ${Calculate.TaxAmount} Discount amount = ${Calculate.DiscountAmount} Price before = ${Book.Price}, price after = ${BookWithTaxAndDiscount.Price} ");
+
+        Console.WriteLine("Tax = 20%, discount = 15%");
+        Product Book1WithTaxAndDiscount = Calculate.AddTaxWithDiscount(Book, 20, 15);
+
+        Console.WriteLine("Tax = 20%, no discount");
+        Product Book2WithTaxAndDiscount = Calculate.AddTaxWithDiscount(Book, 20, 0);
     }
 }
---
+++ Price Calculator Kata/Price Calculator Kata/Report.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Price_Calculator_Kata
+{
+     class Report
+    {
+        public static void DiscountedReport(double FinalPrice, double DiscountAmount)
+        {
+            Console.WriteLine($"Final Price : ${FinalPrice}");
+
+            if (DiscountAmount == 0)
+                return;
+
+            Console.WriteLine($"discounted amount : ${DiscountAmount}");
+
+        }
+
+    }
+}
